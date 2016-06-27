6037449bdcb83f6293615a033040b2c7c42df2f0
Nizam <nyzam7@gmail.com>
2016-06-27 16:26:40 +0300

    Added all pages

--- wp-content/themes/advokat-zv/search.php
+++ wp-content/themes/advokat-zv/search.php
@@ -14,16 +14,18 @@
     <main id="main" class="site-main" role="main">
         <div class="container">
             <?php if (have_posts()) : ?>
-                <?php while (have_posts()) : the_post() ?>
-                    <?php get_template_part('template-parts/content', 'search') ?>
-                <?php endwhile ?>
+                <div class="row">
+                    <?php while (have_posts()) : the_post() ?>
+                        <?php get_template_part('template-parts/content', 'search') ?>
+                    <?php endwhile ?>
+                </div>
                 <div class="text-center">
                     <div class="nav-wrapper">
                         <?php wp_bootstrap_pagination() ?>
                     </div>
                 </div>
             <?php else : ?>
-                <?php get_template_part('template-parts/content', 'none') ?>
+                Нет результатов
             <?php endif ?>
         </div>
     </main><!-- #main -->
